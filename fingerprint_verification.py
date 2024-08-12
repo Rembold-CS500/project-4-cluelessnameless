bac93f6f3b38efe6516a57d57e948f0cf8d15482
Lucie <lygrey96@gmail.com>
2024-08-12 10:04:40 -0700

    code fixed

--- fingerprint_verification.py
+++ fingerprint_verification.py
@@ -52,30 +52,31 @@ def variant_check(fingerprint1, fingerprint2, threshold = .95):
 # shifted check, still checking the variant, but adjusting for changes in x and y both positive & negative
 # max changes should be 5, keeping within the threshold
 # max matches will start at 0, similar to variant
-"""
+
 def shifted_check(fingerprint1, fingerprint2, threshold = .95):
+    max_change = 5
     max_matches = 0
 
     # adjust for change in x and change in y, loop for up down left right (-/+)
-    for dx in range(-len(fingerprint1['fingerprint']), len(fingerprint1['fingerprint'])):
-        for dy in range(-len(fingerprint1['fingerprint'][dx]), len(fingerprint1['fingerprint'][dx])):
+    for dx in range(-max_change, max_change + 1):
+        for dy in range(-max_change, max_change + 1):
             matches = 0
             total = 0
             # loop within like variant, checking rows and within rows
             # the change/shift needs to account for the up/down left/right shifts, add change in x/y to the row, then column
             for i in range(len(fingerprint1['fingerprint'])):
                 for j in range(len(fingerprint1['fingerprint'][i])):
-                    di = i + dx
-                    dj = j + dy
+                    di = i + dy
+                    dj = j + dx
                     if 0 <= di < len(fingerprint1['fingerprint']) and 0 <= dj < len(fingerprint1['fingerprint'][i]):
-                        total += 1
                         if fingerprint1['fingerprint'][i][j] == fingerprint2['fingerprint'][di][dj]:
                             matches += 1
+                        total += 1
             
             if total > 0:
                 max_matches = max(max_matches, (matches / total))
     return max_matches >= threshold
-"""
+
 
 if __name__ == '__main__':
     # Just to get you started
@@ -90,6 +91,10 @@ if __name__ == '__main__':
     fh1_shiftedv2 = './prints/User1_ShiftedVariant2.txt'
 
     fh2 = './prints/User2_Original.txt'
+    fh2_v1 = './prints/User2_Variant1.txt'
+    fh2_v2 = './prints/User2_Variant2.txt'
+    fh2_shiftedv1 = './prints/User2_ShiftedVariant1.txt'
+    fh2_shiftedv2 = './prints/User2_ShiftedVariant2.txt'
 
     fp1 = read_print(fh1)
     fp1_v1 = read_print(fh1_v1)
@@ -98,6 +103,10 @@ if __name__ == '__main__':
     fp1_shiftedv2 = read_print(fh1_shiftedv2)
 
     fp2 = read_print(fh2)
+    fp2_v1 = read_print(fh2_v1)
+    fp2_v2 = read_print(fh2_v2)
+    fp2_shiftedv1 = read_print(fh2_shiftedv1)
+    fp2_shiftedv2 = read_print(fh2_shiftedv2)
 
     # for a simple check
     print("Simple Checks:")
@@ -121,15 +130,19 @@ if __name__ == '__main__':
     # expected False
     print("")
 
-"""
     # for a shifted check
     print("Shifted Checks")
     print(f"Fingerprint match: {shifted_check(fp1,fp1_shiftedv1)}")
-    # expected True - was returning False but why?
+    # expected True - was returning False but why? - was not including the upper range, +1 fix
     print(f"Fingerprint match: {shifted_check(fp1,fp1_shiftedv2)}")
     # expected True
     print(f"Fingerprint match: {shifted_check(fp1_shiftedv1,fp1_shiftedv2)}")
     # expected True    
     print(f"Fingerprint match: {shifted_check(fp1,fp2)}")
-    # expected False - now this returns True but why?
-"""
+    # expected False
+    #print(f"Fingerprint match: {shifted_check(fp2,fp2_shiftedv1)}")
+    # expected True
+    #print(f"Fingerprint match: {shifted_check(fp2,fp2_shiftedv2)}")
+    # expected True
+    #print(f"Fingerprint match: {shifted_check(fp2_shiftedv1,fp2_shiftedv2)}")
+    # expected True
